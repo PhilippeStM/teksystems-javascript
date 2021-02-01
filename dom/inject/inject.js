e859491dea620f2763744591760a6858e3041b0d
phil-stm <philippestm@yahoo.com>
2021-02-01 12:54:10 -0800

    Additional elements added to inject.

--- dom/inject/inject.js
+++ dom/inject/inject.js
@@ -5,16 +5,28 @@ const inject = document.getElementById("inject");
 
 const h1 = document.createElement("h1");
 const h2 = document.createElement("h2");
-const a = document.createElement("a");
-const p = document.createElement("p");
+const p1 = document.createElement("p");
+const p2 = document.createElement("p2");
 const ol = document.createElement("ol");
 const li = document.createElement("li");
+const hr1 = document.createElement("hr");
+const hr2 = document.createElement("hr2");
 
-h1.innerHTML = "HTML: Practice Exercise TEKcamp";
-h2.innerHTML = `TEKsystems "TEKcamp"`;
-p.innerHTML = "I love" + <i>HTML</i> + " for the following reasons:";
+h1.innerHTML = "<center> <i>HTML: Practice Exercise TEKcamp</i> </center>";
+h2.innerHTML = `<center> <a href=""> TEKsystems "TEKcamp" </a> </center>`;
+p.innerHTML = "I love <i>HTML</i> for the following reasons:";
+hr1.innerHTML = "<hr />";
+ol.innerHTML = 
+    `
+    <li> I learned it quickly. </li>
+    <li> I can make web pages using code. </li>
+    <li> It's fun. </li>
+    `;
 
-inject.append(h1, h2, p);
+hr2.innerHTML = "<hr />"
+p2.innerHTML = `My instructor’s email address is: <a href="mailto:ayunas@teksystems.com">ayunas@teksystems.com</a>.`
+
+inject.append(h1, h2, hr1, p, ol, hr2, p2);
 
 
 
@@ -22,7 +34,7 @@ inject.append(h1, h2, p);
 
 
 
-{/* <center>
+/* <center>
 <h1><i>HTML Practice Exercise TEKcamp.</i></h1>
 <h2><a href="">TEKsystems "TEKcamp"</a></h2>
 </center>
@@ -37,4 +49,4 @@ inject.append(h1, h2, p);
 <hr />
 <p>My instructor’s email address is: <a href="mailto:ayunas@teksystems.com">ayunas@teksystems.com</a>.</p>
 <h1>Have a great day!</h1>  I really look forward to learning how to code!  Have a great day.  
--[Team "Git'er Done"] */}
+-[Team "Git'er Done"] */
