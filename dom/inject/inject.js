5f24f1b67592b34c16141775561d6eb13d5a8fef
phil-stm <philippestm@yahoo.com>
2021-02-01 13:09:16 -0800

    Completed inject assignment.

--- dom/inject/inject.js
+++ dom/inject/inject.js
@@ -3,18 +3,20 @@
 
 const inject = document.getElementById("inject");
 
-const h1 = document.createElement("h1");
+const h1One = document.createElement("h1");
+const h1Two = document.createElement("h1");
 const h2 = document.createElement("h2");
 const p1 = document.createElement("p");
-const p2 = document.createElement("p2");
+const p2 = document.createElement("p");
+const p3 = document.createElement("p");
 const ol = document.createElement("ol");
 const li = document.createElement("li");
-const hr1 = document.createElement("hr");
-const hr2 = document.createElement("hr2");
+const hr1 = document.createElement("hr1");
+const hr2 = document.createElement("hr1");
 
-h1.innerHTML = "<center> <i>HTML: Practice Exercise TEKcamp</i> </center>";
+h1One.innerHTML = "<center> <i>HTML: Practice Exercise TEKcamp</i> </center>";
 h2.innerHTML = `<center> <a href=""> TEKsystems "TEKcamp" </a> </center>`;
-p.innerHTML = "I love <i>HTML</i> for the following reasons:";
+p1.innerHTML = "I love <i>HTML</i> for the following reasons:";
 hr1.innerHTML = "<hr />";
 ol.innerHTML = 
     `
@@ -23,30 +25,9 @@ ol.innerHTML =
     <li> It's fun. </li>
     `;
 
-hr2.innerHTML = "<hr />"
-p2.innerHTML = `My instructor’s email address is: <a href="mailto:ayunas@teksystems.com">ayunas@teksystems.com</a>.`
+hr2.innerHTML = "<hr />";
+p2.innerHTML = `My instructor’s email address is: <a href="mailto:ayunas@teksystems.com">ayunas@teksystems.com</a>.`;
+h1Two.innerHTML = "Have a great day!";
+p3.innerHTML = `I really look forward to learning how to code!  Have a great day. -[Team "Git'er Done"]`
 
-inject.append(h1, h2, hr1, p, ol, hr2, p2);
-
-
-
-
-
-
-
-/* <center>
-<h1><i>HTML Practice Exercise TEKcamp.</i></h1>
-<h2><a href="">TEKsystems "TEKcamp"</a></h2>
-</center>
-
-<hr />
-<p>I love <i>HTML</i> for the following reasons:</p>
-<ol>
-<li>I learned it quickly.</li>
-<li>I can make web pages using code
-<li> It’s fun.</li>
-</ol>
-<hr />
-<p>My instructor’s email address is: <a href="mailto:ayunas@teksystems.com">ayunas@teksystems.com</a>.</p>
-<h1>Have a great day!</h1>  I really look forward to learning how to code!  Have a great day.  
--[Team "Git'er Done"] */
+inject.append(h1One, h2, p1, hr1, ol, hr2, p2, h1Two, p3);
