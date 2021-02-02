b5f1b1a2c9a88e2dea9c6d7bffe261c0b87b9b5f
phil-stm <philippestm@yahoo.com>
2021-02-01 17:10:44 -0800

    Started working on timer exercise.

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,2 +1,8 @@
 console.log("time to start the timer!");
 
+const stopwatch = document.getElementById("stopwatch");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+console.log(reset);
