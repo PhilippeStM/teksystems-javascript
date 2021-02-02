7e51d5a6d625838a6dd715875423c584c7bf1291
phil-stm <philippestm@yahoo.com>
2021-02-01 16:59:52 -0800

    Finished shopping list, now starting on timer.

--- dom/shopping/shoppingList.js
+++ dom/shopping/shoppingList.js
@@ -1,10 +1,26 @@
 const ul = document.querySelector("ul");
-const input = document.querySelector("input");
+let input = document.querySelector("input");
 const button = document.querySelector("button");
 
 button.addEventListener("click", function () {
     let newInputValue = document.getElementById("item").value;
-    newInputValue = input;
     console.log(newInputValue);
-})
 
+    document.getElementById("item").value = "";
+
+    const li = document.createElement("li");
+    let span = document.createElement("span");
+    const deleteButton = document.createElement("button");
+
+    li.append(span, deleteButton);
+    span.textContent = newInputValue;
+    deleteButton.textContent = "Delete";
+    
+    ul.append(li);
+
+    deleteButton.onclick = function remove() {
+        li.remove();
+    }
+
+    document.getElementById("item").focus();
+})
